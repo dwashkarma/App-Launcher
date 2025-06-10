527d1ee38b1eaa37be8f9562988cf4af0d613d6d
DwashKarma <dwaskc@gmail.com>
2025-06-10 13:12:25 +0545

    build for browsers

--- rollup.config.js
+++ rollup.config.js
@@ -5,27 +5,54 @@ import commonJs from "@rollup/plugin-commonjs";
 import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 import PeerDepsExternalPlugin from "rollup-plugin-peer-deps-external";
-export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: packageJson.main,
-      format: "cjs",
-      sourcemap: true,
-    },
-    {
-      file: packageJson.module,
-      format: "esm",
-      sourcemap: true,
-    },
-  ],
 
-  plugins: [
-    PeerDepsExternalPlugin(),
-    nodeResolve(),
-    commonJs(),
-    terser(),
-    typescript({ tsconfig: "./tsconfig.json" }),
-  ],
-  external: ["react", "react-dom"],
-};
+export default [
+  // ESM and CJS for npm users
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      PeerDepsExternalPlugin(),
+      nodeResolve(),
+      commonJs(),
+      terser(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+    ],
+    external: ["react", "react-dom", "react-dom/client"],
+  },
+
+  // IIFE for CDN users
+  {
+    input: "src/cdn.tsx", // This should contain the `window.renderAppLauncher = function(...)` code
+    output: {
+      file: "dist/app-launcher.cdn.js",
+      format: "iife",
+      name: "AppLauncherGlobal", // required for IIFE/UMD builds
+      globals: {
+        react: "React",
+        "react-dom": "ReactDOM",
+        "react-dom/client": "ReactDOM",
+      },
+      sourcemap: true,
+    },
+    plugins: [
+      PeerDepsExternalPlugin(),
+      nodeResolve(),
+      commonJs(),
+      terser(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+    ],
+    external: ["react", "react-dom", "react-dom/client"],
+  },
+];
